03c9ff921c6523c3825696abc50b5331fabf6aa3
eiuthanghuynh <thang.huynhle.cit22@eiu.edu.vn>
2024-11-29 14:21:11 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to AnhThangFirstRepo's Java Repository.");
+        System.out.println("Hello Anh Thang\nWelcome to my repository.");
     }
 }
